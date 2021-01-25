bb4a48367ba663915ee4a0f616875b154002fe74
AntoshJkeee <toha_bart@mail.ru>
2021-01-25 22:19:23 +0300

    2-module-1-task completed

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (Number.isInteger(salaries[key])) {
+      sum = sum + salaries[key];
+    }
+  }
+  return sum;
 }
