1fa25a556c4bfd5abf52857651f62274f85f9f9a
AntoshJkeee <toha_bart@mail.ru>
2021-01-27 16:08:33 +0300

    3-module-1-task completed

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -2,6 +2,17 @@
  * @param   {{ name: string, age: number }[]} users
  * @returns {string[]}  объект
  */
+
+
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [vasya, petya, masha];
+
+
 function namify(users) {
-  // ваш код...
-}
+  users.push(vasya.name, petya.name, masha.name);
+  users.splice(0, 3);
+  return users;
+};
